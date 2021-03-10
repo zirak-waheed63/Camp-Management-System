67f768f7ed7be065f334638e1cd2c6406693c44d
zirak-waheed <zirak.waheed@square63.org>
2021-03-10 13:26:48 +0500

    code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,5 @@ class ApplicationController < ActionController::Base
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :middle_name, :last_name, :country, :phone_number, :email, :password) }
-              #devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
         end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :country, :phone_number, :encrypted_password, :email, presence: true
   validates :phone_number, numericality: true, length: {minimum: 10, maximum:15,
-                                                        message: "must be 10-15 digit number."}
+                                                        message: 'must be 10-15 digit number.'}
   validate :password_requirements_are_met
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  validates :email,
+  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "invalid"  }, length: { minimum: 4, maximum: 254 }  
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   def password_requirements_are_met
     rules = {
       ' must contain at least one lowercase letter' => /[a-z]+/,
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     }
 
     rules.each do |message, regex|
-      errors.add( :password, message ) unless password.match( regex )
+      errors.add( :password, message ) unless password.match(regex)
     end
   end
 end

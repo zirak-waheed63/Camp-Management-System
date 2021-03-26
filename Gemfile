776512e43f2d6ca0a4a3c707aabc52a28caf31aa
zirak-waheed <zirak.waheed@square63.org>
2021-03-26 18:03:38 +0500

    Added modal for delete and Dynamic fields

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+
+gem 'rails-ujs', '~> 0.1.0'
+gem 'popper_js', '~> 2.8.6'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,21 @@
+$(document).on('turbolinks:load', function() {
+  document.getElementById('overseas-fields').classList.add('hidden')
+  document.getElementById('officers-fields').classList.add('hidden')
+  $('#overseas').change(function() {
+    if(this.checked == true){
+      document.getElementById('overseas-fields').classList.remove('hidden')
+      }
+    else{
+      document.getElementById('overseas-fields').classList.add('hidden')
+    }
+  });
+
+  $('#officer').change(function() {
+    if(this.checked == true){
+      document.getElementById('officers-fields').classList.remove('hidden')
+      }
+    else{
+      document.getElementById('officers-fields').classList.add('hidden')
+    }
+  });
+})
